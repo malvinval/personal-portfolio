744ecdba50275d4b7dc8bb2d0a5bcbc418675a39
malvinval <malvinval09@gmail.com>
2023-01-10 16:26:04 +0700

    fix: dark theme UI adjustment

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -24,7 +24,7 @@ const HomePage = () => {
         <>
             <Navbar />
 
-            <div className="h-full w-full flex justify-center font-poppins bg-white pt-16">
+            <div className="h-full lg:h-screen w-full flex justify-center font-poppins bg-white pt-16">
                 <Particles id="tsparticles" options={ParticleConfig} init={particlesInit} />
                 <div className="bg-white w-3/4 h-3/4 mt-20 flex flex-col lg:flex-row">
                     <div className="w-full lg:w-1/2 flex justify-center items-center">
