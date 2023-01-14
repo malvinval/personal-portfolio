45536a7b595afc6436cc99c5f5dedc58e679703d
malvinval <malvinval09@gmail.com>
2023-01-14 12:59:46 +0700

    update: about page z-index values

--- src/components/AboutPageTitleAndDesc.jsx
+++ src/components/AboutPageTitleAndDesc.jsx
@@ -3,12 +3,12 @@ import React from "react";
 
 const AboutPageTitleAndDesc = (props) => {
     const element = (
-        <div className="w-full flex flex-col justify-center lg:w-1/2 z-30">
-            <div className="flex justify-center items-center py-5">
+        <div className="w-full flex flex-col justify-center lg:w-1/2">
+            <div className="flex justify-center items-center py-5 z-30">
                 <h1 className="text-center text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#01aae7] to-[#0974f1]">{props.title}</h1>
                 <MdOutlineKeyboardArrowRight className="hidden lg:block text-4xl text-blue-700 font-bold" />
             </div>
-            <div className="w-full lg:w-3/4 mx-auto text-justify">
+            <div className="w-full lg:w-3/4 mx-auto text-justify z-30">
                 <p className="text-gray-500 font-medium leading-8">{props.desc}</p>
             </div>
         </div>
