3c752ee9844d4f2b72b2d9e6e9b40d358d0113bc
malvinval <malvinval09@gmail.com>
2023-03-30 21:20:47 +0700

    update: formspree id, dark theme adjustment

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -9,7 +9,7 @@ const Projects = () => {
     const particlesInit = async (main) => {
         await loadFull(main);
     };
-    const [state, handleSubmit] = useForm("your_formspree_id");
+    const [state, handleSubmit] = useForm("xpzeypkp");
     
     const element = (
         <>
@@ -19,19 +19,19 @@ const Projects = () => {
                 <div className="w-full flex justify-center items-center font-poppins pt-16">
                     <Particles id="tsparticles" options={ParticleConfig} init={particlesInit} />
                     
-                    <section class="bg-white dark:bg-gray-900 z-30">
+                    <section class="bg-white z-30">
                         <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md rounded-lg mt-10 lg:mt-20">
-                            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-transparent bg-clip-text bg-gradient-to-r from-[#01aae7] to-[#0974f1]">Give me a feedback !</h2>
+                            <h2 class="mb-4 text-2xl lg:text-4xl tracking-tight font-extrabold text-center text-transparent bg-clip-text bg-gradient-to-r from-[#01aae7] to-[#0974f1]">Give me a feedback !</h2>
                             <p class="mb-8 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">I would love to receive any feedbacks from you. Send it <span className="underline">anonymously</span> via the form below!</p>
                             <AlertModal isMessageSent={state.succeeded ? "true":""} status="success" title="Your message successfully sent!" />
                             <form onSubmit={handleSubmit} class="space-y-8">
                                 <div>
-                                    <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-                                    <input type="text" id="subject" name="subject" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let me know how I can help you" required />
+                                    <label for="subject" class="block mb-2 text-sm font-medium text-gray-700">Subject</label>
+                                    <input type="text" id="subject" name="subject" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="Let me know how I can help you" required />
                                 </div>
                                 <div class="sm:col-span-2">
-                                    <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-                                    <textarea id="message" name="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..." required></textarea>
+                                    <label for="message" class="block mb-2 text-sm font-medium text-gray-700">Your message</label>
+                                    <textarea id="message" name="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500" placeholder="Leave a comment..." required></textarea>
                                 </div>
                                 <button type="submit" disabled={state.submitting} class="btn btn-primary py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 sm:w-fit focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
                             </form>
