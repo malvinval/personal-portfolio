e074c04dc011f7079b7511d986da5c2272584322
malvinval <malvinval09@gmail.com>
2023-09-19 00:06:15 +0700

    update: company logo size props

--- src/pages/Experiences.jsx
+++ src/pages/Experiences.jsx
@@ -9,7 +9,7 @@ import LabKompUnsikaLogo from "../img/labkompunsika.png";
 import OuseanLogo from "../img/ousean.png";
 import KampusMerdekaLogo from "../img/kampusmerdeka.png";
 import BellshadeLogo from "../img/bellshade.png";
-import OkoceLogo from "../img/OkoceLogo.png";
+import OkoceLogo from "../img/OkoceLogo-2.png";
 
 const Experiences = () => {
     const particlesInit = async (main) => {
@@ -29,11 +29,11 @@ const Experiences = () => {
                             <h1 className="absolute left-0 right-0 text-center text-3xl lg:text-4xl font-extrabold text-gray-800 underline decoration-pink-500">Relevant Experience</h1>
                         </div>
                         <div className="flex flex-col justify-center items-center mt-24 w-11/12 space-y-4 z-30">
-                            <ExperienceItem logo={OkoceLogo} title="Website and Social Media Developer" desc="From August - December 2023, I have been doing an internship at OK OCE Indonesia as a Website and Social Media developer who focuses on developing website applications and it's security." />
-                            <ExperienceItem logo={LabKompUnsikaLogo} title="Laboratorium Assistant" desc="Since January 2023, I have been working as a computer laboratory assistant at University of Singaperbangsa Karawang. My main responsibility is to teach material from several courses such as programming algorithms, object-oriented programming, website programming and website frameworks." />
-                            <ExperienceItem logo={OuseanLogo} title="Web Developer Intern" desc="Since December 2022, I have been doing an internship as a Frontend Web Developer at a startup working in the Education, Health and Human Empowerment sectors, namely PT. Ousean Global Digital. My main responsibility is to build the user interface of the website, and manage the data from the API provided by the backend team." />
-                            <ExperienceItem logo={KampusMerdekaLogo} title="Apprentice Teacher" desc="Since February 2023, I have been working as a teacher at SDN Jatisampurna IV in the 'Kampus Mengajar' program organized by the Ministry of Education and Culture of the Republic of Indonesia. My main task there is to implement a 'Merdeka' curriculum which focuses on numeracy, literacy, administration, science and technology." />
-                            <ExperienceItem logo={BellshadeLogo} title="Repository Contributor" desc="I am one of the contributors to an IT community, namely Bellshade. The community is under the auspices of Web Programming UNPAS which is a source for learning programming in Indonesia. I wrote a lot of programming material there, including HTML & CSS, PHP, SQL, Java, C++, and Javascript." />
+                            <ExperienceItem width="10" height="5" logo={OkoceLogo} title="Website and Social Media Developer" desc="From August - December 2023, I have been doing an internship at OK OCE Indonesia as a Website and Social Media developer who focuses on developing website applications and it's security." />
+                            <ExperienceItem width="10" height="10" logo={LabKompUnsikaLogo} title="Laboratorium Assistant" desc="Since January 2023, I have been working as a computer laboratory assistant at University of Singaperbangsa Karawang. My main responsibility is to teach material from several courses such as programming algorithms, object-oriented programming, website programming and website frameworks." />
+                            <ExperienceItem width="10" height="10" logo={OuseanLogo} title="Web Developer Intern" desc="Since December 2022, I have been doing an internship as a Frontend Web Developer at a startup working in the Education, Health and Human Empowerment sectors, namely PT. Ousean Global Digital. My main responsibility is to build the user interface of the website, and manage the data from the API provided by the backend team." />
+                            <ExperienceItem width="10" height="10" logo={KampusMerdekaLogo} title="Apprentice Teacher" desc="Since February 2023, I have been working as a teacher at SDN Jatisampurna IV in the 'Kampus Mengajar' program organized by the Ministry of Education and Culture of the Republic of Indonesia. My main task there is to implement a 'Merdeka' curriculum which focuses on numeracy, literacy, administration, science and technology." />
+                            <ExperienceItem width="10" height="10" logo={BellshadeLogo} title="Repository Contributor" desc="I am one of the contributors to an IT community, namely Bellshade. The community is under the auspices of Web Programming UNPAS which is a source for learning programming in Indonesia. I wrote a lot of programming material there, including HTML & CSS, PHP, SQL, Java, C++, and Javascript." />
                         </div>
                     </div>
                 </div>
